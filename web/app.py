c8338aca7406a6085194a7100462f9609b2d232b
Иван Бобков <ivbobkov@miem.hse.ru>
2020-04-22 21:01:08 +0300

    web: removing all cameras from camera list available; web-forms reorganization.

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, url_for, redirect
-from forms import CameraListForm
+import cameralist_forms
 import json
 import os
 
@@ -14,17 +14,18 @@ def index():
 
 @app.route('/recorder', methods=['GET', 'POST'])
 def recorder():
-    form = CameraListForm()
+    addForm = cameralist_forms.AddCameraForm()
+    deleteForm = cameralist_forms.DeleteAllForm()
     
     # adding new camera to json
-    if form.validate_on_submit():
+    if addForm.validate_on_submit():
         # writing data to file
         with open('cams.json', 'r+') as json_file:
             data = json.load(json_file)
             temp = data['cams']
             new_cam = {
-                        "name": form.cameraName.data,
-                        "address": form.rtspName.data
+                        "name": addForm.cameraName.data,
+                        "address": addForm.rtspName.data
                     }
             temp.append(new_cam)
             json_file.seek(0)
@@ -32,8 +33,29 @@ def recorder():
             json_file.close()
 
         # web-page success message
-        flash(f'Camera {form.cameraName.data} has been added successfuly!', 'success')
+        flash(f'Camera {addForm.cameraName.data} has been added successfuly!', 'success')
         return redirect(url_for('recorder'))
+    
+
+    # delete all cameras
+    if deleteForm.validate_on_submit():
+        # reading data from file
+        with open('cams.json', 'r') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+        
+        # writing empty data to json
+        with open('cams.json', 'w') as json_file:
+            temp = data['cams']
+            temp.clear()
+            data['cams'] = temp
+            json.dump(data, json_file, indent=4)
+            json_file.close()
+
+        # web-page success message
+        flash(f'All cameras have been removed successfuly', 'success')
+        return redirect(url_for('recorder'))
+
 
     # reading cameras from json
     with open('cams.json') as json_file:
@@ -44,4 +66,4 @@ def recorder():
         for i in range(len(data['cams'])):
             cams.update({data['cams'][i]['name']: data['cams'][i]['address']})
 
-    return render_template('recorder.html', cams=cams, form=form)
+    return render_template('recorder.html', cams=cams, addForm=addForm, deleteForm=deleteForm)
--- web/cameralist_forms.py
+++ web/cameralist_forms.py
@@ -2,7 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class CameraListForm(FlaskForm):
+class AddCameraForm(FlaskForm):
     cameraName = StringField('Camera name', validators=[DataRequired()])
     rtspName = StringField('RTSP address', validators=[DataRequired()])
     submit = SubmitField('Add new camera')
+
+class DeleteAllForm(FlaskForm):
+    submit = SubmitField('Delete all')
